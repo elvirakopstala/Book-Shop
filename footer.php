7d0de647ccc190fb046e64da188d259d22ea54a4
elvirakopstala <elvirakopstala@gmail.com>
2022-08-25 10:02:51 +0300

    book-shop

---
+++ footer.php
@@ -0,0 +1,42 @@
+<section class="footer">
+<hr>
+   <div class="box-container">
+
+      <div class="box">
+         <h3>INFO</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>EXTRA INFO</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <!-- <p> <i class="fas fa-phone"></i></p> <a href="tel:123-456-7890">+123-456-7890</a> -->
+         <a href="tel:123-456-7890"> <i class="fas fa-phone"></i>+123-456-7890</a>
+         <a href="tel:111-222-3333"> <i class="fas fa-phone"></i>+111-222-3333</a>
+         <a href="mailto:book.smart@gmail.com"> <i class="fas fa-envelope"></i>book.smart@gmail.com</a>
+         <p> <i class="fas fa-map-marker-alt"></i> Riga, Latvia </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  <?php echo date('Y'); ?> by <span>Lasma Alise Elvira</span> </p>
+
+</section>
